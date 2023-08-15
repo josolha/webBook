02092c1995179b0a8614e22b6a20848ec5dcb443
josolha <josolha@nate.com>
2023-08-15 23:43:47 +0900

    feat : 회원가입 기능.

--- src/main/java/org/zerock/b01/controller/MemberController.java
+++ src/main/java/org/zerock/b01/controller/MemberController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.MemberJoinDTO;
+import org.zerock.b01.service.MemberService;
 
 @Controller
 @RequestMapping("/member")
@@ -14,23 +16,48 @@ import org.zerock.b01.dto.MemberJoinDTO;
 @RequiredArgsConstructor
 public class MemberController {
 
-    @GetMapping("/login")
-    public void loginGET(String error, String logout){
-        log.info("login get..........");
-        log.info("logout: " + logout);
+    private final MemberService memberService;
 
-        if(logout != null){
-            log.info("user logout.......");
-        }
-    }
     @GetMapping("/join")
     public void joinGET(){
-        log.info("join get..");
+
+        log.info("join get...");
+
     }
+
+//    @PostMapping("/join")
+//    public String joinPOST(MemberJoinDTO memberJoinDTO){
+//
+//        log.info("join post...");
+//        log.info(memberJoinDTO);
+//
+//        return "redirect:/board/list";
+//    }
+//
+
     @PostMapping("/join")
-    public String joinPOST(MemberJoinDTO memberJoinDTO){
+    public String joinPOST(MemberJoinDTO memberJoinDTO, RedirectAttributes redirectAttributes){
+
         log.info("join post...");
         log.info(memberJoinDTO);
-        return "redirect:/board/list";
+
+        try {
+            memberService.join(memberJoinDTO);
+        } catch (MemberService.MidExistException e) {
+
+            redirectAttributes.addFlashAttribute("error", "mid");
+            return "redirect:/member/join";
+        }
+
+        redirectAttributes.addFlashAttribute("result", "success");
+
+        return "redirect:/member/login"; //회원 가입 후 로그인
     }
+
+    @GetMapping("/login")
+    public void loginGET(String error, String logout) {
+        log.info("login get..............");
+        log.info("logout: " + logout);
+    }
+
 }
